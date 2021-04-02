64f0d628914a9b00ec6f3a054de9a9da4c118cc4
Escuela Technologies <escuela.tech@gmail.com>
2021-04-02 09:40:38 -0500

    Updating the latest changes

--- helloworld/src/main/java/com/demo/helloworld/spring/controllers/DemoController.java
+++ helloworld/src/main/java/com/demo/helloworld/spring/controllers/DemoController.java
@@ -3,8 +3,12 @@ package com.demo.helloworld.spring.controllers;
 import com.demo.helloworld.spring.jpa.models.User;
 import com.demo.helloworld.spring.jpa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RequestMapping("user")
@@ -24,7 +28,22 @@ public class DemoController {
 
     @GetMapping("/fetchall")
     public @ResponseBody List<User> getUser(){
-        return userRepository.findAll();
+
+       // userRepository.findAll();
+
+
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<User> users = userRepository.findAll(pageable);
+        System.out.println("********************************************************");
+        System.out.println("Users received = "+users.getSize());
+        System.out.println("Total Pages    = "+users.getTotalPages());
+        System.out.println("********************************************************");
+
+        for(User user:users){
+            System.out.println(user.getFirstName());
+        }
+        return Collections.emptyList();
     }
 
 }
--- helloworld/src/main/java/com/demo/helloworld/spring/jpa/repositories/UserRepository.java
+++ helloworld/src/main/java/com/demo/helloworld/spring/jpa/repositories/UserRepository.java
@@ -2,8 +2,9 @@ package com.demo.helloworld.spring.jpa.repositories;
 
 import com.demo.helloworld.spring.jpa.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User,Integer> {
 }
